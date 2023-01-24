4b40f6f0077ebaaa1b38318b8eaf56d74dd2548d
eanggaa <eanggawahyu@gmail.com>
2023-01-24 14:16:14 +0700

    table

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::get('/sosialisasi', function() { return view('sosialisasi.index'); })->name('sosialisasi');
+    Route::get('/pelatihan-a', function() { return view('pelatihan-a.index'); })->name('pelatihan-a');
+    Route::get('/pelatihan-b', function() { return view('pelatihan-b.index'); })->name('pelatihan-b');
+    Route::get('/pelatihan-c', function() { return view('pelatihan-c.index'); })->name('pelatihan-c');
+    Route::get('/pendampingan', function() { return view('pendampingan.index'); })->name('pendampingan');
+    Route::get('/apresiasi', function() { return view('apresiasi.index'); })->name('apresiasi');
 });
+
