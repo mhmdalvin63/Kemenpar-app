f61e3b3e8e257fc94d2afbf79de8262e8bedae6a
muhammadalvin92 <alvinbomber87@gmail.com>
2023-01-24 14:37:50 +0700

    Exclude Kegiatan

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+
+Route::get('/tambahNarasumber', function () {return view('Narasumber.listNarasumber');});
+Route::get('/desaWisata', function () {return view('desaWisata.desaWisata');});
+Route::get('/hotel', function () {return view('hotel.hotel');});
+Route::get('/proposalDesa', function () {return view('proposalDesa.proposalDesa');});
+Route::get('/laporanBulanan', function () {return view('laporanBulanan.laporanBulanan');});
+
+
+
+
+
+
