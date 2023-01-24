d397956dd83d74c66275934de641f9259818ed5a
eanggaa <eanggawahyu@gmail.com>
2023-01-24 15:11:55 +0700

    judul table

--- resources/views/pelatihan-c/index.blade.php
+++ resources/views/pelatihan-c/index.blade.php
@@ -36,11 +36,13 @@
                 <th class="text-center">
                   <i class="fas fa-th"></i>
                 </th>
-                <th>Task Name</th>
-                <th>Progress</th>
-                <th>Members</th>
-                <th>Due Date</th>
-                <th>Status</th>
+                <th>Materi Narasumber 1</th>
+                <th>Materi Narasumber 2</th>
+                <th>Materi Narasumber 3</th>
+                <th>Daftar Peserta</th>
+                <th>Evaluasi Pelatihan</th>
+                <th>Evaluasi Narasumber</th>
+                <th>Lampiran</th>
                 <th>Action</th>
               </tr>
             </thead>
